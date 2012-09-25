c43980e36a4620bd584b0afd9b53787bec81e17f
Jonathan Stout <jonstout@indiana.edu>
2012-09-24 22:08:13 -0400

    Added initial nox code

--- overlord.py
+++ overlord.py
@@ -32,3 +32,22 @@ def get_message(msg):
         pass
     else:
         pass
+
+def launch():
+    core.openflow.addListenerByName("ConnectionUp", _handleConnectionUp)
+    core.openflow.addListenerByName("PacketIn", _handlePacketIn)
+
+def _handleConnectionUp(event):
+    msg = of.ofp_flow_mod()
+    msg.actions.append(of.ofp_action_output())
+    event.connection.send(msg)
+
+def _handlePacketIn(event):
+    packet = event.parsed
+    if (not source_exists(packet.src)):
+        # Install drop action on mac_src
+        msg = of.ofp_flow_mod()
+        msg.match.dl_src = packet.src
+        event.connection.send(msg)
+        # Add device information to db
+        overlord.learn_device(packet)
