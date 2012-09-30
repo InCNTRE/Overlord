dc5000c00014d178feb78b6f1e71b1e209257627
Jonathan Stout <jonstout@indiana.edu>
2012-09-30 18:45:53 -0400

    Basic layout for arping

--- Overlord/Hosts/Hosts.py
+++ Overlord/Hosts/Hosts.py
@@ -11,10 +11,42 @@ class Hosts(object):
 
         # ARP Packet
         if pkt.type == 2054:
+            # Learn about the new Device
             arp_pkt = pkt.next
             self.memorizeHost(event.dpid, arp_pkt.hwsrc, arp_pkt.protosrc, event.port)
             log.debug(str(event.dpid)+' '+str(arp_pkt.hwsrc)+' '+str(arp_pkt.protosrc)+' '+str(event.port))
+            # What group is he in? Find from database
+            group_no = self.getHostGroup(arp_pkt.hwsrc)
+            if group_no == -2:
+                # New device, add to default -1 drop group in db
+                # Install Drop(hwsrc, hwdst)
+                pass
+            elif group_no == -1:
+                # Already in defautl drop group
+                # Install Drop(hwsrc, hwdst)
+                pass
+            else:
+                if self.knownHost(arp_pkt.hwdst):
+                    if group_no == self.getHostGroup(arp_pkt.hwdst):
+                        # Add forwarding rules
+                        pass
+                    else:
+                        # They're not defined to talk together
+                        # Install Drop(hwsrc, hwdst)
+                        # Install Drop(hwdst, hwsrc)
+                        pass
+                else:
+                    # Not sure where or if they exist
+                    # Arp the whole group_no group
+                    # Install Drop(hwsrc, hwdst)
+                    pass
+                    
+    def getHostGroup(self, hwaddr):
+        return 0
 
+    def knownHost(self, hwaddr):
+        """binary search self.known_hosts"""
+        return True
 
     def memorizeHost(self, dpid, mac, ip, port):
         """Yeah insertion sort sucks here, but fuck it.
