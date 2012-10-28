8c8e0129e7e88e8995d73909b7f076ea51a9d7f7
Jonathan Stout <jonstout@indiana.edu>
2012-10-28 15:51:11 -0400

    major messup, though closer to what I want.

--- Overlord/Forwarding/Forwarding.py
+++ Overlord/Forwarding/Forwarding.py
@@ -31,6 +31,24 @@ class Forwarding(object):
             fmod.actions.append(of.ofp_action_output(port=int(host1["port_no"])))
             event.connection.send(fmod)
 
+            #forward arp too :P. When I move to RPC calls remove this and
+            # match on dl_src and dl_dst
+            msg = of.ofp_flow_mod(idle_timeout=5)
+            msg.match.dl_src = EthAddr(host1["mac"])#arp_pkt.hwsrc
+            msg.match.dl_type = 0x0806
+            msg.match.dl_dst = EthAddr(host2["mac"])#arp_pkt.hwdst
+            msg.actions.append(of.ofp_action_output(port=int(host2["port_no"])))
+            event.connection.send(msg)
+
+            msg = of.ofp_flow_mod(idle_timeout=5)
+            #msg.match.dl_src = arp_pkt.hwsrc
+            msg.match.dl_src = EthAddr(host2["mac"])#arp_pkt.hwsrc
+            msg.match.dl_type = 0x0806
+            #msg.match.protodst = arp_pkt.protodst
+            msg.match.dl_dst = EthAddr(host1["mac"])#arp_pkt.hwdst
+            msg.actions.append(of.ofp_action_output(port=int(host1["port_no"])))
+            event.connection.send(msg)
+
         log.info("Adding flows to connect devices: " + str(host1["_name"]) + " and " + str(host2["_name"]))
 
     def Disconnect(self, log, event, host):
@@ -47,6 +65,23 @@ class Forwarding(object):
         #fmod.match.dl_dst = EthAddr(host["mac"])
         fmod.command = of.OFPFC_DELETE
         event.connection.send(fmod)
+
+        fmod = of.ofp_flow_mod()
+        fmod.match.dl_type = 0x0806
+        #fmod.match.nw_dst = IPAddr(str(host["ip"]))
+        fmod.match.dl_dst = EthAddr(host["mac"])
+        fmod.command = of.OFPFC_DELETE
+        event.connection.send(fmod)
+        
+        fmod = of.ofp_flow_mod()
+        fmod.match.dl_type = 0x0806
+        #fmod.match.nw_dst = IPAddr(str(host["ip"]))
+        fmod.match.dl_dst = EthAddr(host["mac"])
+        fmod.command = of.OFPFC_DELETE
+        event.connection.send(fmod)
+
+        
+
         log.info("Removing flows to disconnect device: " + str(host["_name"]))
 
     def Group(self):
--- Overlord/Hosts/Hosts.py
+++ Overlord/Hosts/Hosts.py
@@ -25,6 +25,14 @@ class Hosts(object):
         else:
             return None
 
+    def setHostGroupLocal(self, log, mac, group_no):
+        for h in self.known_hosts:
+            if h[0] == mac:
+                del(h)
+                self.known_hosts.append((mac, group_no))
+                print(self.known_hosts)
+                return
+
     def setHostGroup(self, log, db, mac, group_no):
         host = db.hosts.find_one({"mac": str(mac)})
         host["group_no"] = group_no
@@ -46,6 +54,7 @@ class Hosts(object):
         except ValueError:
             return False
 
+    # Returns a tuple (db_data, last_local_known_group)
     def memorizeHost(self, log, db, dpid, port, ip, mac):
         """
         It makes more since to use something like a
@@ -57,99 +66,72 @@ class Hosts(object):
         need to over optimize atm.
         [(mac, {'dpid':x, 'ip':x, 'port':x})]"""
 
-        for h in self.known_hosts:
-            if h == str(mac):
-                host = db.hosts.find_one({"mac": str(mac)})
-                host["_parent"] = str(dpid)
-                host["port_no"] = str(port)
-                host["ip"] = str(ip)
-                host["mac"] = str(mac)
-                db.hosts.save(host)
-                return host
-        
-        self.known_hosts.append(str(mac))
-        data = {"_parent": str(dpid), "port_no": str(port), "ip": str(ip), "mac": str(mac), "group_no": "-1", "group_was": "-1"}
-        db.hosts.save(data)
-        return data
-        """
-        i = 0
-        while i < len(self.known_hosts) and mac >= self.known_hosts[i]:
-            i += 1
-        # Makes sure macs aren't duplicated, only updated
-        if i == 0 or mac != self.known_hosts[i-1]:
-            log.debug('Memorizing ' + str(self.known_hosts))
-            self.known_hosts.insert(i, mac)
-            log.debug('Discovered Host: '+str(mac)+' '+str(ip))
-        
-        # Save the dpid, ip, and port to the database here incase something changed
         host = db.hosts.find_one({"mac": str(mac)})
+
         if host == None:
-            data = {"_parent": str(dpid), "port_no": str(port), "ip": str(ip), "mac": str(mac)}#, "group_no": "-1"})
+            # If the source mac is not known add a local (mac, group) entry,
+            #  and push the known information to the database.
+            self.known_hosts.append( (str(mac), "-1") )
+            data = {"_parent": str(dpid), "port_no": str(port), "ip": str(ip), "mac": str(mac), "group_no": "-1"}
             db.hosts.save(data)
-            return data
+            self.known_hosts.append(str(mac), "-a")
+            return (data, "-1")
         else:
-            host["_parent"] = str(dpid)
-            host["port_no"] = str(port)
-            host["ip"] = str(ip)
-            host["mac"] = str(mac)
-            db.hosts.save(host)
-            return host"""
+            for h in self.known_hosts:
+                if h[0] == str(mac):
+                    host["_parent"] = str(dpid)
+                    host["port_no"] = str(port)
+                    host["ip"] = str(ip)
+                    host["mac"] = str(mac)
+                    db.hosts.save(host)
+                    return (host, str(h[1]))
+
+        # The host is known but not recorded locally. Save and Return.
+        self.known_hosts.append((str(host["mac"]), str(host["group_no"])))
+        return (host, str(host["group_no"]))
+
 
     def learnArp(self, log, db, forwarding, links, event, pkt):
-        # Learn the host information
+        # Learn or Update the host information
         arp_pkt = pkt.next
-        host1 = self.memorizeHost(log, db, event.dpid, event.port, arp_pkt.protosrc, arp_pkt.hwsrc)
+        host1, last_group = self.memorizeHost(log, db, event.dpid, event.port, arp_pkt.protosrc, arp_pkt.hwsrc)
         
-        # Find out what group the device is in. If he's even in one.
-        group_no = host1["group_no"]#self.getHostGroup(log, db, arp_pkt.hwsrc)
-        if group_no == "-1":
-            # Drop everything from this source for 3 seconds (group changes may occour)
-            msg = of.ofp_flow_mod(hard_timeout=3)
+        # If the group_no in the database doesn't match what we know Disconnect
+        #  this host from all others.
+        print(str(host1["group_no"]) + " " + last_group)
+        if str(host1["group_no"]) != str(last_group):
+            forwarding.Disconnect(log, event, host1)
+            self.setHostGroupLocal(log, str(host1["mac"]), str(host1["group_no"]))
+
+        if str(host1["group_no"]) == "-1":
+            # Drop all IP traffic from this host
+            msg = of.ofp_flow_mod(idle_timeout=300)
             msg.match.dl_src = arp_pkt.hwsrc
-            msg.match.dl_type = 0x0806
-            msg.match.protodst = arp_pkt.protodst
+            msg.match.dl_type = 0x0800
             event.connection.send(msg)
-        elif group_no != None:
-            # Forward to group members that are known
-            # if protodst is known and group_nos are equal:
-            #     send arp reply
-            #     and build connection
-            #host1 = db.hosts.find_one({"mac": str(arp_pkt.hwsrc)})
+        else:
             host2 = db.hosts.find_one({"ip": str(arp_pkt.protodst)})
+            if host2 == None: return
 
             log.debug(str(host1))
             log.debug(str(host2))
 
-            if host2 != None:
-                # Something changed so remove all previous flow regarding this device
-                forwarding.Disconnect(log, event, host1)
+            # ARPing a device that is known to exist and has the same group permissions
+            if str(host1["group_no"]) == str(host2["group_no"]):
                 # Rebuild the links between the device and group members
-                for h in self.getGroupMembers(log, db, arp_pkt.hwsrc, group_no):
-                    log.debug(str(host1) + str(host2))
-                    forwarding.Connect(log, links, event, host1, h)
+                #for h in self.getGroupMembers(log, db, arp_pkt.hwsrc, group_no):
+                #    log.debug(str(host1) + str(h))
+                forwarding.Connect(log, links, event, host1, host2)
+                #self.setHostGroupLocal(host1["mac"], host1["group_no"])
                     #     send arp reply
-                    amsg = arp()
-                    amsg.opcode = 2
-                    amsg.hwsrc = EthAddr(h["mac"])
-                    amsg.hwdst = EthAddr(host1["mac"])
-                    amsg.protosrc = IPAddr(str(h["ip"]))
-                    amsg.prododst = IPAddr(str(host1["ip"]))
-                    pkt.next = amsg
-                    pkt_out= of.ofp_packet_out()
-                    pkt_out.actions.append(of.ofp_action_output(port=int(host1["port_no"])))
-                    pkt_out.data = pkt
-                    event.connection.send(pkt_out)
-            else:
-                # Drop everything from this source
-                msg = of.ofp_flow_mod(hard_timeout=3)
-                msg.match.dl_src = arp_pkt.hwsrc
-                msg.match.protodst = arp_pkt.protodst
-                event.connection.send(msg)
-        else:
-            # Update group_no to group -1
-            self.setHostGroup(log, db, arp_pkt.hwsrc, "-1")
-            # Drop everything from this source
-            msg = of.ofp_flow_mod(hard_timeout=3)
-            msg.match.dl_src = arp_pkt.hwsrc
-            msg.match.protodst = arp_pkt.protodst
-            event.connection.send(msg)
+                amsg = arp()
+                amsg.opcode = 2
+                amsg.hwsrc = EthAddr(host2["mac"])
+                amsg.hwdst = EthAddr(host1["mac"])
+                amsg.protosrc = IPAddr(str(host2["ip"]))
+                amsg.prododst = IPAddr(str(host1["ip"]))
+                pkt.next = amsg
+                pkt_out= of.ofp_packet_out()
+                pkt_out.actions.append(of.ofp_action_output(port=int(host1["port_no"])))
+                pkt_out.data = pkt
+                event.connection.send(pkt_out)
