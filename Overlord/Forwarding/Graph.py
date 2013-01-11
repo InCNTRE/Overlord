01fd14451dcc0f5b66fe9bf3c52b7181f6fb648d
Jonathan Stout <jonstout@indiana.edu>
2013-01-11 16:56:16 -0500

    began migration to graph.py

--- Overlord/Forwarding/Graph.py
+++ Overlord/Forwarding/Graph.py
@@ -1,6 +1,76 @@
 # @org InCNTRE 2013
 # @author Jonathan Stout    
 
+from Path import *
+from PathSolution import dijkstra
+from ..Lib.Events import Event, Eventful
+
+class Graph(Eventful):
+    def __init__(self):
+        self.path_nodes = {}
+        self.paths = {}
+        self.switches = {}
+
+        self.path_id = self.id_generator()
+
+        self.add_event("path_down", self.handle_path_down)
+        self.add_event("path_mod", self.handle_path_mod)
+        self.add_event("path_up", self.handle_path_up)
+
+    def add_link(self, rcvr_dpid, sndr_dpid, in_port):
+        """
+        Adds a link to a Switch objects internal link structure.
+        @ param rcvr_dpid Dpid of recieving device
+        @ param sndr_dpid Dpid of sending device
+        @ param in_port Port number of recieving device
+        """
+        self.switches[rcvr_dpid].add_link(sndr_dpid, in_port)
+
+    def add_switch(self, dpid):
+        """
+        Adds a switch object to the network graph.
+        @param dpid Dpid of new switch connection
+        """
+        self.switches[dpid] = Switch(dpid)
+
+    def down_switch(self, dpid):
+        """
+        Triggers event causing the recalculation of links.
+        @param dpid Dpid of device connection that went down
+        """
+        try:
+            self.path_nodes[dpid].node_down()
+        except KeyError:
+            print("No path involoving {} has been provisioned" \
+                      .format( str(dpid) ))
+
+    def get_path(self, dpid_a, dpid_b, path_id=None):
+        """
+        Returns a Path object containing PathNodes for
+        forwarding rules to be based upon.
+        @param dpid_a Dpid of first host device
+        @param dpid_b Dpid of second host device
+        @param path_id ID of path
+        """
+        #TODO:: Update this code to build path
+        if path_id == None:
+            path_id = self.path_id
+
+    def id_generator(self):
+        pid = 1
+        while True:
+            pid += 1
+            yield pid
+
+    def handle_path_down(self, e):
+        pass
+
+    def handle_path_mod(self, e):
+        pass
+
+    def handle_path_up(self, e):
+        pass
+
 class Switch(object):
     def __init__(self, dpid):
         self.dpid = dpid
